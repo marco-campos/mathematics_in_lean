6a5d7862f0beb2339b8c7ee82d604b85debaa5be
Marco Campos <marco@Marcos-MacBook-Pro-2.local>
2024-12-24 22:56:32 -0600

    finished section 3

--- C03_Logic/S05_Disjunction.lean
+++ C03_Logic/S05_Disjunction.lean
@@ -1,5 +1,6 @@
 import MIL.Common
 import Mathlib.Data.Real.Basic
+import Paperproof
 
 namespace C03S05
 
@@ -22,6 +23,7 @@ example (h : y < -1) : y > 0 ∨ y < -1 :=
   Or.inr h
 
 example : x < |y| → x < y ∨ x < -y := by
+-- Here we have to use h | h because we have a disjunction (or statement)
   rcases le_or_gt 0 y with h | h
   · rw [abs_of_nonneg h]
     intro h; left; exact h
@@ -58,19 +60,68 @@ example : x < |y| → x < y ∨ x < -y := by
 namespace MyAbs
 
 theorem le_abs_self (x : ℝ) : x ≤ |x| := by
-  sorry
+  rcases le_or_gt 0 x with h | h
+  · rw [abs_of_nonneg h]
+  · rw [abs_of_neg h]
+    linarith
 
 theorem neg_le_abs_self (x : ℝ) : -x ≤ |x| := by
-  sorry
+  rcases le_or_gt 0 x with h | h
+  · rw [abs_of_nonneg h]
+    linarith
+  · rw [abs_of_neg h]
+
 
 theorem abs_add (x y : ℝ) : |x + y| ≤ |x| + |y| := by
-  sorry
+  rcases le_or_gt 0 (x + y) with h | h
+  · rw [abs_of_nonneg h]
+    linarith [le_abs_self x, le_abs_self y]
+  rw [abs_of_neg h]
+  linarith [neg_le_abs_self x, neg_le_abs_self y]
+
 
 theorem lt_abs : x < |y| ↔ x < y ∨ x < -y := by
-  sorry
+  rcases le_or_gt 0 y with h|h
+  · rw [abs_of_nonneg h]
+    constructor
+    · intro h1
+      left
+      apply h1
+    intro h2
+    rcases h2 with h3 | h3
+    apply h3
+    linarith
+  rw [abs_of_neg h]
+  constructor
+  · intro h'
+    right
+    apply h'
+  intro h4
+  rcases h4 with h5 | h5
+  linarith
+  apply h5
 
 theorem abs_lt : |x| < y ↔ -y < x ∧ x < y := by
-  sorry
+  rcases le_or_gt 0 x with h|h
+  · constructor
+    · rw [abs_of_nonneg h]
+      intro h1
+      constructor
+      linarith
+      apply h1
+    · intro h1
+      rw [abs_of_nonneg h]
+      rcases h1 with ⟨h2, h3⟩
+      apply h3
+  constructor
+  · rw [abs_of_neg h]
+    intro h1
+    constructor
+    linarith
+    linarith
+  rw [abs_of_neg h]
+  intro h1
+  linarith
 
 end MyAbs
 
@@ -91,23 +142,58 @@ example {m n k : ℕ} (h : m ∣ n ∨ m ∣ k) : m ∣ n * k := by
     apply dvd_mul_right
 
 example {z : ℝ} (h : ∃ x y, z = x ^ 2 + y ^ 2 ∨ z = x ^ 2 + y ^ 2 + 1) : z ≥ 0 := by
-  sorry
+  rcases h with ⟨x, y, rfl | rfl⟩ <;> linarith [sq_nonneg x, sq_nonneg y]
 
 example {x : ℝ} (h : x ^ 2 = 1) : x = 1 ∨ x = -1 := by
-  sorry
+  have h' : x ^ 2 - 1 = 0 := by rw [h, sub_self]
+  have h'' : (x + 1) * (x - 1) = 0 := by
+    rw [← h']
+    ring
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero h'' with h1 | h2
+  · right
+    linarith
+  · left
+    linarith
 
 example {x y : ℝ} (h : x ^ 2 = y ^ 2) : x = y ∨ x = -y := by
-  sorry
+  have h' : x ^2 - y ^ 2 = 0 := by rw [h, sub_self]
+  have h'' : (x - y) * (x + y) = 0 := by
+    rw [← h']
+    ring
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero h'' with h1 | h2
+  · left
+    linarith
+  · right
+    linarith
 
 section
 variable {R : Type*} [CommRing R] [IsDomain R]
 variable (x y : R)
 
 example (h : x ^ 2 = 1) : x = 1 ∨ x = -1 := by
-  sorry
+  have h' : x ^ 2 - 1 = 0 := by rw [h, sub_self]
+  have h'' : (x + 1) * (x - 1) = 0 := by
+    rw [← h']
+    ring
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero h'' with h1 | h2
+  · right
+    apply eq_neg_iff_add_eq_zero.mpr
+    apply h1
+  · left
+    apply eq_of_sub_eq_zero
+    apply h2
 
 example (h : x ^ 2 = y ^ 2) : x = y ∨ x = -y := by
-  sorry
+  have h' : x ^2 - y ^ 2 = 0 := by rw [h, sub_self]
+  have h'' : (x + y) * (x - y) = 0 := by
+    rw [← h']
+    ring
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero h'' with h1 | h2
+  · right
+    exact eq_neg_iff_add_eq_zero.mpr h1
+  · left
+    apply eq_of_sub_eq_zero
+    apply h2
 
 end
 
@@ -124,5 +210,15 @@ example (P : Prop) : ¬¬P → P := by
   contradiction
 
 example (P Q : Prop) : P → Q ↔ ¬P ∨ Q := by
-  sorry
-
+  constructor
+  · intro h
+    by_cases h' : P
+    · right
+      exact h h'
+    · left
+      apply h'
+  rintro (h | h)
+  intro h'
+  exact absurd h' h
+  intro h'
+  apply h
--- C03_Logic/S06_Sequences_and_Convergence.lean
+++ C03_Logic/S06_Sequences_and_Convergence.lean
@@ -1,5 +1,6 @@
 import MIL.Common
 import Mathlib.Data.Real.Basic
+import Paperproof
 
 namespace C03S06
 
@@ -20,9 +21,13 @@ example {a : ℝ} (h : 1 < a) : a < a * a := by
   exact lt_trans zero_lt_one h
 
 theorem convergesTo_const (a : ℝ) : ConvergesTo (fun x : ℕ ↦ a) a := by
+  -- This comes from ConvergesTo. We need to show that for all ε such that ε > 0
   intro ε εpos
+  -- Here we just use N = 0
   use 0
+  -- introduced any n ∈ ℕ and the condition n ≥ N = 0
   intro n nge
+  -- Now we just have to reduce this which eventually boils down to ε > 0.
   rw [sub_self, abs_zero]
   apply εpos
 
@@ -32,10 +37,22 @@ theorem convergesTo_add {s t : ℕ → ℝ} {a b : ℝ}
   intro ε εpos
   dsimp -- this line is not needed but cleans up the goal a bit.
   have ε2pos : 0 < ε / 2 := by linarith
+  -- for our given ε, there exists Ns and Nt for cs and ct respectively
+  -- such that for all n ≥ Ns and n ≥ Nt, |s n - a| < ε /2....
   rcases cs (ε / 2) ε2pos with ⟨Ns, hs⟩
   rcases ct (ε / 2) ε2pos with ⟨Nt, ht⟩
   use max Ns Nt
-  sorry
+  intro n nge
+  have ngeNs : n ≥ Ns := le_of_max_le_left nge
+  have ngeNt : n ≥ Nt := le_of_max_le_right nge
+  calc
+    |s n + t n  - (a + b)| = |(s n - a) + (t n - b)| := by
+      congr
+      ring
+    -- Triangle inequality.
+    _ ≤ |s n - a| + |t n - b| := (abs_add _ _)
+    _ < ε / 2 + ε / 2 := (add_lt_add (hs n ngeNs) (ht n ngeNt))
+    _ = ε := by norm_num
 
 theorem convergesTo_mul_const {s : ℕ → ℝ} {a : ℝ} (c : ℝ) (cs : ConvergesTo s a) :
     ConvergesTo (fun n ↦ c * s n) (c * a) := by
@@ -46,13 +63,32 @@ theorem convergesTo_mul_const {s : ℕ → ℝ} {a : ℝ} (c : ℝ) (cs : Conver
     rw [h]
     ring
   have acpos : 0 < |c| := abs_pos.mpr h
-  sorry
+  intro ε εpos
+  dsimp
+  have εcpos : 0 < ε / |c| := by apply div_pos εpos acpos
+  rcases cs (ε / |c|) εcpos with ⟨Ns, hs⟩
+  use Ns
+  intro n ngt
+  calc
+    |c * s n - c * a| = |c| * |s n - a| := by
+      rw [← abs_mul, mul_sub]
+    _ < |c| * (ε / |c|) := (mul_lt_mul_of_pos_left (hs n ngt) acpos)
+    _ = ε := mul_div_cancel₀ _ (ne_of_lt acpos).symm
+
 
 theorem exists_abs_le_of_convergesTo {s : ℕ → ℝ} {a : ℝ} (cs : ConvergesTo s a) :
     ∃ N b, ∀ n, N ≤ n → |s n| < b := by
   rcases cs 1 zero_lt_one with ⟨N, h⟩
   use N, |a| + 1
-  sorry
+  -- |s n - a + a| ≤ |s n - a| + |a| < ε + |a|
+  -- so if we let ε = 1, then we have what we need.
+  intro n ngt
+  calc
+    |s n| = |s n - a + a| := by
+      congr
+      abel
+    _ ≤ |s n - a| + |a| := (abs_add _ _)
+    _ < |a| + 1 := by linarith [h n ngt]
 
 theorem aux {s t : ℕ → ℝ} {a : ℝ} (cs : ConvergesTo s a) (ct : ConvergesTo t 0) :
     ConvergesTo (fun n ↦ s n * t n) 0 := by
@@ -62,7 +98,17 @@ theorem aux {s t : ℕ → ℝ} {a : ℝ} (cs : ConvergesTo s a) (ct : Converges
   have Bpos : 0 < B := lt_of_le_of_lt (abs_nonneg _) (h₀ N₀ (le_refl _))
   have pos₀ : ε / B > 0 := div_pos εpos Bpos
   rcases ct _ pos₀ with ⟨N₁, h₁⟩
-  sorry
+  use max N₀ N₁
+  intro n ngt
+  have ngeN₀ : n ≥ N₀ := le_of_max_le_left ngt
+  have ngeN₁ : n ≥ N₁ := le_of_max_le_right ngt
+  calc
+    |s n * t n - 0| = |s n| * |t n - 0| := by
+      rw [sub_zero, abs_mul, sub_zero]
+    _ < B * (ε / B) :=
+      (mul_lt_mul'' (h₀ n ngeN₀) (h₁ n ngeN₁) (abs_nonneg _) (abs_nonneg _))
+    _ = ε := mul_div_cancel₀ _ (ne_of_lt Bpos).symm
+
 
 theorem convergesTo_mul {s t : ℕ → ℝ} {a b : ℝ}
       (cs : ConvergesTo s a) (ct : ConvergesTo t b) :
@@ -80,7 +126,14 @@ theorem convergesTo_unique {s : ℕ → ℝ} {a b : ℝ}
       (sa : ConvergesTo s a) (sb : ConvergesTo s b) :
     a = b := by
   by_contra abne
-  have : |a - b| > 0 := by sorry
+  have : |a - b| > 0 := by
+    apply lt_of_le_of_ne
+    · apply abs_nonneg
+    intro h''
+    apply abne
+    apply eq_of_abs_sub_eq_zero
+    apply h''.symm
+
   let ε := |a - b| / 2
   have εpos : ε > 0 := by
     change |a - b| / 2 > 0
@@ -88,9 +141,21 @@ theorem convergesTo_unique {s : ℕ → ℝ} {a b : ℝ}
   rcases sa ε εpos with ⟨Na, hNa⟩
   rcases sb ε εpos with ⟨Nb, hNb⟩
   let N := max Na Nb
-  have absa : |s N - a| < ε := by sorry
-  have absb : |s N - b| < ε := by sorry
-  have : |a - b| < |a - b| := by sorry
+  have absa : |s N - a| < ε := by
+    apply hNa
+    apply le_max_left
+  have absb : |s N - b| < ε := by
+    apply hNb
+    apply le_max_right
+  have : |a - b| < |a - b| :=
+  calc
+    |a - b| = |(-(s N -a)) + (s N - b)| := by
+      congr
+      ring
+    _ ≤ |(-(s N -a))| + |s N - b| := (abs_add _ _)
+    _ = |s N - a| + |s N - b| := by rw [abs_neg]
+    _ < ε + ε := (add_lt_add absa absb)
+    _ = |a - b|:= by norm_num [ε]
   exact lt_irrefl _ this
 
 section
@@ -100,4 +165,3 @@ def ConvergesTo' (s : α → ℝ) (a : ℝ) :=
   ∀ ε > 0, ∃ N, ∀ n ≥ N, |s n - a| < ε
 
 end
-
